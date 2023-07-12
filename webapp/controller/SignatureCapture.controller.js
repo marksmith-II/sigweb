7121f58983bd307d1d17d67ea869da7637342a2b
Ethan Jewett <esjewett@gmail.com>
2023-07-12 17:19:58 -0500

    Setup of SigWebSDK - not quite loading correctly yet

--- webapp/controller/SignatureCapture.controller.js
+++ webapp/controller/SignatureCapture.controller.js
@@ -1,15 +1,36 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageBox) {
         "use strict";
 
         return Controller.extend("com.borderstates.topazsignature.controller.SignatureCapture", {
-            onInit: function () {
+            sigWebLoaded: function () {
+                if(!document.IsSigWebInstalled) {
+                    MessageBox.error("Unable to load the SigWeb library.");
+                    return;
+                }
 
+                if(!document.IsSigWebInstalled()) {
+                    MessageBox.error("SigWebSDK must be installed on this computer.");
+                    return;
+                }
+
+                // All the business logic goes here.
+            },
+
+            onInit: function () {
+                if(!document.IsSigWebInstalled) {
+                    var callback = this.sigWebLoaded;
+                    var sigWebScript = document.createElement('script');
+                    sigWebScript.setAttribute('src','../lib/SigWebTablet.js');
+                    sigWebScript.onload = function() { setTimeout(callback, 1000) };
+                    document.head.appendChild(sigWebScript);
+                }
             }
         });
     });
