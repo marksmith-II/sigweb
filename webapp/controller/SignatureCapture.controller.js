5bf3914430a3fe58c6fbb8c724c677814d183911
Mark Smith <markssmith2@gmail.com>
2023-07-17 18:13:30 -0600

    Images and Testing

--- webapp/controller/SignatureCapture.controller.js
+++ webapp/controller/SignatureCapture.controller.js
@@ -21,6 +21,38 @@ sap.ui.define([
                 }
 
                 // All the business logic goes here.
+
+
+
+	function startTablet()
+	{
+	
+		
+		
+			SetTabletState(1);
+			var tabletState = GetTabletState();
+			    LcdRefresh(0, 0, 0, 640, 480);
+				ClearSigWindow(1);
+				// SetDisplayXSize( 640 );
+				// SetDisplayYSize( 33 );
+				// SetImageXSize(640);
+				// SetImageYSize(33);
+
+				 SetLCDCaptureMode( 2 );
+				 LCDSetPixelDepth(8);
+				 LCDSendGraphicUrl(1, 2, 0, 0, "https://github.com/MindsetConsulting/com.borderstates.topazsignature/blob/main/webapp/images/Delivery_Details%20.bmp");
+				LCDSetPixelDepth(8);
+				LCDWriteString(0, 2, 20, 375, "20pt Verdana", 27, "Tablet Instanciated");
+				 LCDSetWindow(0, 0, 1, 1);
+				 SetSigWindow(1, 0, 0, 1, 1);
+				// KeyPadAddHotSpot(0, 1, 20, 370, 300, 50);
+				 SetLCDCaptureMode( 2 );
+			
+
+	}
+    startTablet();
+ 
+                 
             },
 
             onInit: function () {
