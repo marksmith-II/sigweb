43130111095efbc015e22e594f52b8e23c67372f
Mark Smith <markssmith2@gmail.com>
2023-07-26 11:11:16 -0600

    code clean up

--- webapp/controller/SignatureCapture.controller.js
+++ webapp/controller/SignatureCapture.controller.js
@@ -4,14 +4,14 @@ sap.ui.define(
     "sap/m/MessageBox",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
-    "com/borderstates/topazsignature/lib/SigWebTablet"
+    "com/borderstates/topazsignature/lib/SigWebTablet",
   ],
 
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
 
-  function (Controller, MessageBox, Filter, FilterOperator, SigWebTablet) {
+  function (Controller, MessageBox, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend(
@@ -32,27 +32,12 @@ sap.ui.define(
         // All the business logic goes here.
 
         startTablet: function () {
-            SetTabletState(1);
+          SetTabletState(1);
           //   var tabletState = GetTabletState();
           LcdRefresh(0, 0, 0, 640, 480);
-          LCDSendGraphicUrl(
-            1,
-            2,
-            0,
-            0,
-            "http://localhost:8081/images/Delivery_Details .bmp"
-          );
-          // LCDSendGraphicUrl( 1, 2, 0, 0, "http://localhost:8080/images/Delivery_Details%20.bmp");
+          LCDSendGraphicUrl(1, 2, 0, 0, "http://localhost:8081/images/Delivery_Details .bmp");
           LCDSetPixelDepth(8);
-          LCDWriteString(
-            0,
-            2,
-            20,
-            375,
-            "20pt Verdana",
-            27,
-            "Tablet Instanciated"
-          );
+          LCDWriteString(0, 2, 20, 375, "20pt Verdana", 27, "Tablet Instanciated");
         },
 
         // deliveryHeaderScreen: function () {},
@@ -60,33 +45,15 @@ sap.ui.define(
         // signatureHeaderScreen: function () {},
 
         acceptButton: function () {
-            LCDSendGraphicUrl(
-            0,
-            2,
-            450,
-            375,
-            "http://localhost:8081/images/Accept_Button.bmp"
-          );
+          LCDSendGraphicUrl( 0, 2, 450, 375, "http://localhost:8081/images/Accept_Button.bmp"  );
           KeyPadAddHotSpot(0, 2, 450, 370, 135, 75);
         },
         cancelButton: function () {
-            LCDSendGraphicUrl(
-            0,
-            2,
-            50,
-            375,
-            "http://localhost:8081/images/Cancel_Button.bmp"
-          );
+          LCDSendGraphicUrl( 0, 2, 50, 375, "http://localhost:8081/images/Cancel_Button.bmp"  );
           KeyPadAddHotSpot(1, 2, 45, 375, 135, 75);
         },
         clearButton: function () {
-            LCDSendGraphicUrl(
-            0,
-            2,
-            260,
-            375,
-            "http://localhost:8081/images/Clear_Button.bmp"
-          );
+          LCDSendGraphicUrl(  0,  2, 260,  375, "http://localhost:8081/images/Clear_Button.bmp"  );
           KeyPadAddHotSpot(2, 2, 260, 375, 135, 75);
         },
 
@@ -98,17 +65,14 @@ sap.ui.define(
           oPromise
             .then(
               function () {
-                this.getDeliveryItems(80003607, "msmith", "G00d@lien1"); // next function
+                this.getDeliveryItems(80002005, "msmith", "G00d@lien1"); // next function
               }.bind(this)
             )
             .catch(function (err) {
-                console.log(err);
+              console.log(err);
               // reject(err); // error handling
               //   MessageToast.show(err);
             });
-
-          //   this.startTablet();
-          //   this.getDeliveryItems(80003607, "msmith", "G00d@lien1");
         },
 
         getDeliveryItems: function (documentNumber, username, password) {
@@ -134,7 +98,7 @@ sap.ui.define(
           oDataModel.read("/" + "DeliveryItems", {
             headers: headers,
             filters: aFilters,
-            urlParameters: sUrlParam,
+            // urlParameters: sUrlParam,
             async: true,
             success: function (oData) {
               console.log(oData);
@@ -145,11 +109,9 @@ sap.ui.define(
           });
         },
 
-        // startTablet();
-        // getDeliveryItems(80000023, "msmith", "G00d@lien1");
-
         onInit: function () {
           if (!document.IsSigWebInstalled) {
+            // Do something here to check if SigWeb is installed
             // var callback = this.sigWebLoaded;
             // var sigWebScript = document.createElement("script");
             // sigWebScript.setAttribute("src", "../lib/SigWebTablet.js");
@@ -167,13 +129,12 @@ sap.ui.define(
                 // next function
               }.bind(this)
             )
-            .then(function () {}.bind(this)) // add these to add more functions
-            .then(function () {}.bind(this))
-            .then(function () {}.bind(this))
+            .then(function () { }.bind(this)) // add these to add more functions
+            .then(function () { }.bind(this))
+            .then(function () { }.bind(this))
             .catch(function (err) {
-                console.log(err);
+              console.log(err);
               // reject(err); // error handling
-              //   MessageToast.show(err);
             });
         },
       }
