7b685146bd1ef99f915a4d25647530e3b2933303
Mark Smith <markssmith2@gmail.com>
2023-07-29 09:28:51 -0600

    2nd screen

--- webapp/controller/SignatureCapture.controller.js
+++ webapp/controller/SignatureCapture.controller.js
@@ -36,13 +36,15 @@ sap.ui.define(
           SetTabletState(1);
           //   var tabletState = GetTabletState();
           LcdRefresh(0, 0, 0, 640, 480);
-          LCDSendGraphicUrl(1, 2, 0, 0, "webapp/images/Delivery_Details .bmp");
+          
           LCDSetPixelDepth(8);
           LCDWriteString(0, 2, 20, 375, "20pt Verdana", 27, "Tablet Instanciated");
         },
         displayDeliveryItems: function (delItemsJSON) {
+          this.deliveryDetailsScreen();
           SetTabletState(1);
           LcdRefresh(0, 0, 0, 640, 480);
+          deliveryHeaderScreen
 
           var ypos = + 50;
           var xposDelivery = 50;
@@ -57,31 +59,161 @@ sap.ui.define(
               var itemQuantity = deliveryDoc.ActualDeliveryQuantity;
               var itemString = "Item: " + Material;
               var quantityString = "Quantity: " + itemQuantity;
-        
+
               LCDSetPixelDepth(8);
               //Item Text
-              LCDWriteString(0, 2, xposDelivery , ypos, "20pt ARIAL", 30, itemString);
+              LCDWriteString(0, 2, xposDelivery, ypos, "20pt ARIAL", 30, itemString);
               //Quantity Text
               LCDWriteString(0, 2, xposQuantity, ypos, "20pt ARIAL", 30, quantityString);
               ypos += 50; // increment the y position
             }
           }
         },
-        // deliveryHeaderScreen: function () {},
-        // customerCertHeaderScreen: function () {},
-        // signatureHeaderScreen: function () {},
+         deliveryDetailsScreen: function () {
+          
+          LcdRefresh(0, 0, 0, 640, 480);
+          LCDSendGraphicUrl(1, 2, 0, 0, "webapp/images/Delivery_Details .bmp");
+
+         },
+         customerCertHeaderScreen: function () {},
+         signatureHeaderScreen: function () {},
 
         acceptButton: function () {
-          LCDSendGraphicUrl(0, 2, 450, 375, "http://localhost:8081/images/Accept_Button.bmp");
+          LCDSendGraphicUrl(0, 2, 450, 375, "../images/Accept_Button.bmp");
           KeyPadAddHotSpot(0, 2, 450, 370, 135, 75);
         },
         cancelButton: function () {
-          LCDSendGraphicUrl(0, 2, 50, 375, "http://localhost:8081/images/Cancel_Button.bmp");
+          LCDSendGraphicUrl(0, 2, 50, 375, "../images/Cancel_Button.bmp");
           KeyPadAddHotSpot(1, 2, 45, 375, 135, 75);
+
+
         },
         clearButton: function () {
-          LCDSendGraphicUrl(0, 2, 260, 375, "http://localhost:8081/images/Clear_Button.bmp");
+          LCDSendGraphicUrl(0, 2, 260, 375, "../images/Clear_Button.bmp");
           KeyPadAddHotSpot(2, 2, 260, 375, 135, 75);
+        },
+        displayCustomerCertStatement: function () {
+          let string1 = 'The Customer Signature Certifies the Material';
+          let string2 = 'described herein are being used in construction';
+          let string3 = 'of the improvments for the referenced project';
+
+          LCDSetPixelDepth(8);
+          LCDWriteString(0, 2, 8, 100, "20pt ARIAL", 30, string1);
+          LCDWriteString(0, 2, 10, 150, "20pt ARIAL", 30, string2);
+          LCDWriteString(0, 2, 10, 200, "20pt ARIAL", 30, string3);
+
+
+        },
+        customerCertScreen: function () {
+          displayCustomerCertStatement();
+          acceptButton();
+          cancelButton();
+          // Screen2ButtonListener();
+          let acceptClicked = 0;
+          let cancelClicked = 0;
+          let buttonValue = this.screenButtonListener(acceptClicked, cancelClicked);
+
+          // Handle button value
+          if (buttonValue.acceptClicked) {
+            // Go to signature screen
+          } else if (buttonValue.cancelClicked) {
+            // Handle cancel click
+
+            sap.m.MessageBox.show("Customer has canceled Delivery", {
+              icon: sap.m.MessageBox.Icon.WARNING,
+              title: "Cancel",
+              actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
+              onClose: function (oAction) {
+                if (oAction === sap.m.MessageBox.Action.YES) {
+
+                }
+              }
+            });
+
+
+          }
+
+
+        },
+        signatureScreen: function () {
+
+          acceptButton();
+          cancelButton();
+          clearButton();
+          // Screen2ButtonListener();
+          let acceptClicked = 0;
+          let cancelClicked = 0;
+          let clearButtonClicked = 0;
+          let buttonValue = this.screenButtonListener(acceptClicked, cancelClicked, clearButtonClicked);
+
+          // Handle button value
+          if (buttonValue.acceptClicked) {
+            // Go to signature screen
+          } else if (buttonValue.cancelClicked) {
+            // Handle cancel click
+
+            sap.m.MessageBox.show("Customer has canceled Delivery", {
+              icon: sap.m.MessageBox.Icon.WARNING,
+              title: "Cancel",
+              actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
+              onClose: function (oAction) {
+                if (oAction === sap.m.MessageBox.Action.YES) {
+
+                } else if (buttonValue.clearClicked) {
+                  signatureScreen();
+                }
+              }
+            });
+
+
+          }
+
+
+
+
+        },
+        screenButtonListener: function () {
+
+          function checkHotspots() {
+            // Check hotspots
+            let acceptClicked = KeyPadQueryHotSpot(0);
+            let cancelClicked = KeyPadQueryHotSpot(1);
+            let clearClicked = KeyPadQueryHotSpot(2);
+
+            // Check if any hotspots were clicked
+            if (acceptClicked > 0 || cancelClicked > 0 || clearClicked > 0) {
+              // Handle click event
+              if (acceptClicked > 0) {
+                // Handle accept click
+                return acceptClicked;
+              } else if (cancelClicked > 0) {
+                // Handle cancel click
+                return cancelClicked;
+              } else if (clearClicked > 0) {
+                // Handle clear click
+                return clearClicked;
+              }
+            } else {
+              // Hotspots not clicked, wait and check again
+              setTimeout(checkHotspots, 100);
+            }
+          }
+
+          // Start checking hotspots
+          checkHotspots();
+
+        },
+        Screen2ButtonListener: function (acceptClicked, cancelClicked, clearClicked) {
+          acceptClicked = false;
+          cancelClicked = false;
+          clearClicked = false;
+
+        },
+        Screen3ButtonListener: function (acceptClicked, cancelClicked, clearClicked) {
+          acceptClicked = false;
+          cancelClicked = false;
+          clearClicked = false;
+
         },
 
         startSigProcess: function () {
@@ -89,7 +221,7 @@ sap.ui.define(
             this.startTablet();
             resolve(true); // return value replaces true
           });
-        
+
           oPromise
             .then(
               function () {
@@ -118,7 +250,7 @@ sap.ui.define(
               // reject(err); // error handling
               //   MessageToast.show(err);
             });
-        
+
           // Define the resolve method outside of the then block
           function resolve(value) {
             console.log(value);
@@ -152,8 +284,8 @@ sap.ui.define(
             };
             oDataModel.read("/" + "DeliveryItems", {
 
-               headers: headers,
-               filters: aFilters,
+              headers: headers,
+              filters: aFilters,
               // urlParameters: sUrlParam,
               async: true,
               success: function (oData) {
